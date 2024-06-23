27f83f090b1ba35a3baffb91baba914bce38ba24
Asim-ITB <asim.itbridge@gmail.com>
2024-06-23 11:18:07 +0545

    disabled dashboard widget

--- database/migrations/2024_06_23_051257_update_galleries_table.php
+++ database/migrations/2024_06_23_051257_update_galleries_table.php
@@ -1,26 +1,40 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateGalleriesTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::table('galleries', function (Blueprint $table) {
             if (!Schema::hasColumn('galleries', 'photo_path')) {
                 $table->string('photo_path')->nullable()->after('title');
             }
+
+            // Add the video_url column if it doesn't exist
             if (!Schema::hasColumn('galleries', 'video_url')) {
                 $table->string('video_url')->nullable()->after('photo_path');
             }
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::table('galleries', function (Blueprint $table) {
-            $table->dropColumn(['photo_path', 'video_url']);
+            if (Schema::hasColumn('galleries', 'photo_path')) {
+                $table->dropColumn('photo_path');
+            }
+            if (Schema::hasColumn('galleries', 'video_url')) {
+                $table->dropColumn('video_url');
+            }
         });
     }
-}
+};
